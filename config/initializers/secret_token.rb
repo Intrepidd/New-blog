1a44d0eb4f1f863b15b4b9d9fda3452eb18f3fda
Intrepidd <adrien.siami@gmail.com>
2011-08-28 10:09:43 +0200

    Importing rails3 w/ some dependencies in the GemFile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewBlog::Application.config.secret_token = 'bceffa260a01a9a64a3af7ee46b08489999d98af551a5793bc63471853a9f5a6bd46027b3bb6f81167fa48cfb0d176732299f2797da81974438fba44819f7d82'
