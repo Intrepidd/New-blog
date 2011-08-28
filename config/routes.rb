1af9b6b6486134ee4812376faaa90f860cc0be91
Intrepidd <adrien.siami@gmail.com>
2011-08-28 10:39:03 +0200

    Removed rails default page and redirected / to HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ NewBlog::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match '/' => 'home#index'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
