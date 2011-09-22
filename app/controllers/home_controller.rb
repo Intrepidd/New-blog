8338b321ee1d74c3635712ba82fa3832c76925d2
Intrepidd <adrien.siami@gmail.com>
2011-09-22 18:41:28 +0200

    Beginning of displaying posts in the home page, missing Markdown for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
 
   def index
+    #get posts
+    page = params[:page].to_i
+    page = 1 if page < 1
+    offset = (page - 1) * 10
+    @posts = Post.find(:all, :order => "created_at DESC", :limit => 10, :offset => offset)
   end
 
 end
